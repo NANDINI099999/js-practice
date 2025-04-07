6505055c7bf88bee7125e03451dba17e7661ac83
NANDINI099999 <155536084+NANDINI099999@users.noreply.github.com>
2025-04-07 16:26:31 +0000

    data types

---
+++ basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // trat all JS code as newer version
+
+// alert(3+3)// we are using nodejs, not browser
+
+console.log(3
+    +3
+); // code readability should be high
+
+console.log("Nandini")
+
+//data types
+
+let name = "Nandini" // string
+let age = 18 //number
+let isLoggedIn = true //boolean
+
+// number => 2^53
+//bigint - for larger values like in stock markets
+//null => standalone value
+// undefined => value not assigned
+// symbol => unique 
+
+// non-primitve
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
