573cc788e71863b3e1f12e114dcf68e06bebfd8b
NANDINI099999 <155536084+NANDINI099999@users.noreply.github.com>
2025-04-11 06:58:04 +0000

    conversion of data types

---
+++ basics/conversionOperation.js
@@ -0,0 +1,40 @@
+
+// convert in number
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueNumber = Number(score)
+//console.log(typeof valueNumber)
+//console.log(valueNumber);
+
+// "78" => 78
+// "78abc" => NaN
+// true => 1; false=> 0
+// null => 0
+
+
+//convert in boolean
+let isLoggedIn = undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1,2,3,..=> true; 0=> false
+// -3 => true
+//null => false
+// undefined => false
+// "nandini" => true
+
+
+//convert in string
+
+let someNumber = 78
+
+let string = String(someNumber)
+console.log(someNumber);
+console.log(typeof someNumber);
+console.log(typeof string);
+
+
