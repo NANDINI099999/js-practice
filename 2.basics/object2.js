3fbf77cbd3b8910d75e01d9a21c7d0a7cd0065da
NANDINI099999 <155536084+NANDINI099999@users.noreply.github.com>
2025-04-27 18:18:19 +0000

    destructuring and JSON

--- 2.basics/object2.js
+++ 2.basics/object2.js
@@ -59,12 +59,77 @@ const dbUser = [
 
 // lets practice methods 
 
-console.log(appUser2);
+//console.log(appUser2);
 
-console.log(Object.keys(appUser2)); // it will return the keys of the object in the form of array which we can use to iterate over the object
+//console.log(Object.keys(appUser2)); // it will return the keys of the object in the form of array which we can use to iterate over the object
 
-console.log(Object.values(appUser2)); // it will return the values of the object
+//console.log(Object.values(appUser2)); // it will return the values of the object
 
-console.log(Object.entries(appUser2)); // it will return the key value pair of the object in the form of array inside an array
+//console.log(Object.entries(appUser2)); // it will return the key value pair of the object in the form of array inside an array
 
-console.log(appUser2.hasOwnProperty("name")); // hasOwnProperty is used to check if the object has the property or not. It will return true or false
+//console.log(appUser2.hasOwnProperty("name")); // hasOwnProperty is used to check if the object has the property or not. It will return true or false
+
+//////////////////////////////////////////////////////////////////
+
+const course = {
+    courseName: "React",
+    courseFee: "900",
+    courseInstructor: "Nandini"
+}
+
+// console.log(course.courseInstructor);
+
+const {courseName: name} /*kya destructure krna hai*/ = course /*kaha se destructure krna hai*/
+ console.log(name);
+ 
+ //destructuring is a technique or way to unpack the values from arrays or objects 
+
+
+ // destructuring in react 
+ const navbar = ({company}) => {
+
+ }
+
+ navbar(company = "Nandini")
+
+ // destrucuring in array
+
+ let des = [1,2,3]
+ let [a,b,c] = des
+ console.log(b);
+ 
+ /* API (Application Programming Interface) = means talking to another srvice to get data.  API is like a menu at a restaurant:
+You don't need to know how they cook your food (complex backend stuff).
+You just order (request) and boom — they serve it to you (response).
+*/
+
+
+ // two ways API shows in js
+ /*
+ 1. Browser API = built in stuff that browser gives you like - fetch()= to get data from INTERNET;localstorage = to save tiny data inside your browser; DOM API = when you do document.querySelector(), you're using an API to interact with the webpage
+
+ 2. External API= Fetching from the INTERNET 
+  Asking other websites for data.
+(Example: "Hey Weather Website, can you tell me the weather today?")
+
+ */
+
+ // Pehle values xml ke format me aati thi  jo bht complex hota tha abhi mostly values json me aati hai
+
+ // json format ex- 
+
+ /**
+  { 
+      "name" : "Nandini",
+      "courseName": "js in hindi"
+      "price" : "free"
+ }
+
+           or,
+
+ [
+    {},
+    {},
+    {}
+ ]
+  */
